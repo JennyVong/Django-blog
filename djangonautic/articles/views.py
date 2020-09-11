f3256bf064f6fad66ebaa8f882f9be1b834087a4
JennyVong <jennyvong502@gmail.com>
2020-09-10 23:59:41 -0400

    made a create Article form

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -9,4 +11,15 @@ def article_list(request):
 def article_detail(request,slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
-    return render(request,'articles/article_detail.html',{'article':article})
+    return render(request,'articles/article_detail.html',{'articles':article})
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST)
+        if form.is_valid():
+            # save to db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request,'articles/article_create.html', {'form':form})
